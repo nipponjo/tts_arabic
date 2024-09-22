d239011acbf7a676158693c06cf25b72cf333383
nipponjo <28433296+nipponjo@users.noreply.github.com>
2024-09-22 17:28:38 +0200

    update readme / test

--- test.py
+++ test.py
@@ -21,14 +21,16 @@ wave = tts(
     text, # input text
     speaker = 1, # speaker id; choose between 0,1,2,3
     pace = 1, # speaker pace
-    denoise = 0.005, # HiFiGAN denoiser strength
+    denoise = 0.005, # vocoder denoiser strength
+    play = True, # play audio?
     pitch_mul = 1, # pitch multiplier
     pitch_add = 0, # pitch offset
-    play = True, # play audio?
+    vowelizer = None, # vowelizer model
+    model_id = 'fastpitch', # Model ID for Text->Mel model
+    vocoder_id = 'hifigan', # Model ID for vocoder model
+    cuda = None, # Optional; CUDA device index
     save_to = './test.wav', # Optionally; save audio WAV file
     bits_per_sample = 32, # when save_to is specified (8, 16 or 32 bits)
-    vowelizer = None, # vowelizer model
-    cuda = True, # use CUDA provider?
     )
 
 # %%
